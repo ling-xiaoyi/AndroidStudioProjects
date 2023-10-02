069478c3314b332aa376a17b427428b286b7a94b
ling-xiaoyi <102516520+ling-xiaoyi@users.noreply.github.com>
2023-10-02 22:33:55 +0800

    app我的页面
    bug：顶部有大约100dp的留白暂未解决

---
+++ APP/app/src/main/java/top/hilyy/git/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,41 @@
+package top.hilyy.git.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import top.hilyy.git.R;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import top.hilyy.git.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    private WebView mWebView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        FragmentNotificationsBinding binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        // 获取 WebView 控件
+        mWebView = root.findViewById(R.id.wv_webview);
+
+        // 设置 WebView 客户端
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // 开启 JavaScript 支持（可选）
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        // 加载网页
+        mWebView.loadUrl("https://git.hilyy.top/lingxiaoyi/index0.html");
+
+        return root;
+    }
+}
---
+++ APP/app/src/main/java/top/hilyy/git/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,11 @@
+package top.hilyy.git.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+
+}
+
